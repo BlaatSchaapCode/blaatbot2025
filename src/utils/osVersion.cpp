41c3b4b851c68ba7a6e7c4afb35f3c968e9b13ec
André van Schoubroeck <andre@blaatschaap.be>
2025-05-27 23:07:41 +0200

    ReactOS and wine detection

--- src/utils/osVersion.cpp
+++ src/utils/osVersion.cpp
@@ -2,25 +2,40 @@
 
 #if defined(_WIN32) || defined(_WIN64)
 
-#include <dbghelp.h>
 #include <windows.h>
+
+#include <dbghelp.h>
 
 #include <string>
 
 void detectWindowsVersion(void) {
-    OSVERSIONINFOW osversioninfow = {0};
-    osversioninfow.dwOSVersionInfoSize = sizeof(osversioninfow);
-    GetVersionExW(&osversioninfow);
+    OSVERSIONINFOA osversioninfoa = {0};
+    osversioninfoa.dwOSVersionInfoSize = sizeof(osversioninfoa);
+    GetVersionExA(&osversioninfoa);
 
-    OSVERSIONINFOEXW osversioninfoexw = {0};
-    osversioninfoexw.dwOSVersionInfoSize = sizeof(osversioninfoexw);
-    GetVersionExW((LPOSVERSIONINFOW)&osversioninfoexw);
+    printf("GetVersionExW Reported Windows version %d.%d.%d platform id %d\n", osversioninfoa.dwMajorVersion,
+           osversioninfoa.dwMinorVersion, osversioninfoa.dwBuildNumber, osversioninfoa.dwPlatformId);
+
+    //    OSVERSIONINFOEXA osversioninfoexa = {0};
+    //    osversioninfoexa.dwOSVersionInfoSize = sizeof(osversioninfoexa);
+    //    GetVersionExA((LPOSVERSIONINFOW)&osversioninfoexa);
+
+    // ReactOS detect
+    // https://reactos.org/forum/viewtopic.php?t=1401
+    char reactos[128] = {};
+    strncpy(reactos, osversioninfoa.szCSDVersion + strlen(osversioninfoa.szCSDVersion) + 1,
+            sizeof(osversioninfoa.szCSDVersion) - (strlen(osversioninfoa.szCSDVersion) + 1));
+
+    if (strlen(reactos)) {
+        printf("Running on %s\n", reactos);
+    } else {
+        printf("Not running on ReactOS\n");
+    }
 
     // If the application has no manifest this will return NT 6.2 (Windows 8)
     // on any later releases.
-    // Hmmm... what is going on? I am getting the **correct** version
-    // on a MINGW64 UCRT64 build. Does it auto-matifest? as I haven't
-    // included any manifest yet. Well.. that makes life easier, doesn't it?
+    // MSYS64 builds automatically include a manifest
+    // Cross compiled builds do not
 
     // Alternatively, we can query the kernel directly. However, this only
     // works if our application is built for the same architecture as the
@@ -33,29 +48,28 @@ void detectWindowsVersion(void) {
     RTL_OSVERSIONINFOW rtl_osversioninfow = {0};
     rtl_osversioninfow.dwOSVersionInfoSize = sizeof(rtl_osversioninfow);
 
-    RTL_OSVERSIONINFOEXW rtl_osversioninfoexw = {0};
-    rtl_osversioninfoexw.dwOSVersionInfoSize = sizeof(rtl_osversioninfoexw);
-
     auto kernel_library_handle = LoadLibrary("NtosKrnl.exe");
     if (kernel_library_handle) {
         typedef NTSTATUS (*RtlGetVersion_f)(PRTL_OSVERSIONINFOW lpVersionInformation);
         RtlGetVersion_f rtlGetVersion = (RtlGetVersion_f)GetProcAddress(kernel_library_handle, "RtlGetVersion");
 
-        typedef NTSTATUS (*RtlGetVersionEx_f)(PRTL_OSVERSIONINFOEXW lpVersionInformation);
-        RtlGetVersionEx_f rtlGetVersionEx = (RtlGetVersionEx_f)GetProcAddress(kernel_library_handle, "RtlGetVersion");
-
         if (rtlGetVersion) {
             rtlGetVersion(&rtl_osversioninfow);
-            rtlGetVersionEx(&rtl_osversioninfoexw);
+
+            printf("RtlGetVersion Reported Windows version %d.%d.%d platform id %d\n", rtl_osversioninfow.dwMajorVersion,
+                   rtl_osversioninfow.dwMinorVersion, rtl_osversioninfow.dwBuildNumber, rtl_osversioninfow.dwPlatformId);
+
         } else {
             // We opened the kernel as a library, but we were unable to obtain
             // the symbol. RtlGetVersion was added in Windows 2000 (NT 5.0)
             // Are we running on NT 4 or NT 3 ?
             // (We are on NT because we were able to load the NT kernel_library_handle)
+            printf("RtlGetVersion not found in kernel\n");
         }
     } else {
         // Unable to open kernel as a library,
         // might be running a different architecture?
+        printf("Unable to load kernel as a library\n");
     }
 
     // Let's have a look at determining the architecture.
@@ -66,6 +80,8 @@ void detectWindowsVersion(void) {
     GetNativeSystemInfo(&native_system_info);
     // When running an x86_64 (AMD64) binary on an aarch64 (ARM64) kernel
     // calling the API to get the architecture lies to us.
+    printf("GetSystemInfo report ProcessorArchitecture as       %x\n", system_info.wProcessorArchitecture);
+    printf("GetNativeSystemInfo report ProcessorArchitecture as %x\n", native_system_info.wProcessorArchitecture);
     // Might want to test IsWow64Process2 too?
 
     // Alternatively, when running on NT, we can look at the kernel's binary
@@ -73,9 +89,13 @@ void detectWindowsVersion(void) {
 
     IMAGE_NT_HEADERS peHdr = {};
     wchar_t windowsDirectory[PATH_MAX + 1] = {};
-    GetWindowsDirectoryW(windowsDirectory, PATH_MAX);
+    // GetWindowsDirectoryW(windowsDirectory, PATH_MAX);
+    GetSystemWindowsDirectoryW(windowsDirectory, PATH_MAX);
     // Is the NT Kernel **always** NtosKrnl.exe or do we need to query the file name?
-    std::wstring windowsKernelPath = std::wstring(windowsDirectory) + L"\\NtosKrnl.exe";
+    std::wstring windowsKernelPath = std::wstring(windowsDirectory) + L"\\system32\\NtosKrnl.exe";
+
+    printf("Windows directory is %ls\n", windowsDirectory);
+    printf("Kernel is %ls\n", windowsKernelPath.c_str());
 
     HANDLE hFile =
         CreateFileW(windowsKernelPath.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_READONLY, NULL);
@@ -87,11 +107,95 @@ void detectWindowsVersion(void) {
                 PIMAGE_NT_HEADERS ppeHdr = ImageNtHeader(addrHeader);
                 if (ppeHdr) {
                     peHdr = *ppeHdr;
+                    printf("Machine is %x\n", peHdr.FileHeader.Machine);
+                } else {
+                    printf("Unable to extract information from kernel\n");
                 }
             }
             CloseHandle(hMapping);
+        } else {
+            printf("Unable to map kernel\n");
         }
         CloseHandle(hFile);
+    } else {
+        printf("Unable to open kernel as a file\n");
+    }
+
+    // Another source we could query for version information is the
+    // registry. However, these values can be edited by the user.
+    // https://learn.microsoft.com/en-us/windows/win32/api/winreg/nf-winreg-reggetvaluea
+    DWORD pcbData = 0;
+
+    // Windows XP:
+    // SZ CurrentVersion
+    // SZ CurrentBuildNumber
+    // SZ CSDVersion
+    // SZ ProductName
+
+
+    // Windows 10:
+    // DWORD CurrentMajorVersionNumber;
+    // DWORD CurrentMinorVersionNumber;
+    // SZ CurrentBuildNumber
+    // SZ CSDVersion
+    // SZ ProductName
+
+
+    // https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes--0-499-
+    // ERROR_FILE_NOT_FOUND
+    DWORD CurrentMajorVersionNumber = 0;
+    pcbData = sizeof(CurrentMajorVersionNumber);
+    LSTATUS status = RegGetValueA(HKEY_LOCAL_MACHINE,                             // hkey
+                                  "SOFTWARE\Microsoft\Windows NT\CurrentVersion", // lpSubKey
+                                  "CurrentMajorVersionNumber",                    // lpValue
+                                  RRF_RT_DWORD | RRF_ZEROONFAILURE,               // dwFlags
+                                  nullptr,                                        // pdwType
+                                  &CurrentMajorVersionNumber,                     // pvData
+                                  &pcbData                                        // pcbData
+    );
+    printf("Reg CurrentMajorVersionNumber status %x Value %d\n", status, CurrentMajorVersionNumber);
+
+    char CurrentVersion[32] = {};
+    pcbData = sizeof(CurrentVersion);
+    status = RegGetValueA(HKEY_LOCAL_MACHINE,                                // hkey
+                          "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", // lpSubKey
+                          "CurrentVersion",                                  // lpValue
+                          RRF_RT_REG_SZ | RRF_ZEROONFAILURE,                 // dwFlags
+                          nullptr,                                           // pdwType
+                          &CurrentVersion,                                   // pvData
+                          &pcbData                                           // pcbData
+    );
+    printf("Reg CurrentVersion status %x Value %s\n", status, CurrentVersion);
+
+    // Checking for wine
+    // https://gist.github.com/klange/282e62fdd1c2cae6a210443ccbc5b12f
+    // https://gist.github.com/ork/32da69687c94530931ed
+
+    typedef const char *(*wine_get_version_f)(void);
+    typedef const char *(*wine_get_build_id_f)(void);
+    typedef const char *(*wine_get_host_version_f)(const char **sysname, const char **release);
+
+    wine_get_version_f wine_get_version = nullptr;
+    wine_get_host_version_f wine_get_host_version = nullptr;
+    wine_get_build_id_f wine_get_build_id = nullptr;
+
+    HMODULE hntdll = GetModuleHandle("ntdll.dll");
+    if (hntdll) {
+        wine_get_version = (wine_get_version_f)GetProcAddress(hntdll, "wine_get_version");
+        wine_get_host_version = (wine_get_host_version_f)GetProcAddress(hntdll, "wine_get_host_version");
+        // wine_get_build_id = (wine_get_build_id_f)GetProcAddress(hntdll, "wine_get_build_id_f");
+        //  wine_get_build_id
+
+        if (wine_get_version && wine_get_host_version) {
+            const char *sysname;
+            const char *version;
+            wine_get_host_version(&sysname, &version);
+            fprintf(stdout, "Running Wine %s under %s %s.\n", wine_get_version(), sysname, version);
+            // Outputs "Running Wine 10.8 under Linux 6.14.6-arch1-1."
+            // fprintf(stdout, "Build ID  %s.\n", wine_get_build_id());
+        } else {
+            fprintf(stdout, "Not running under wine.\n");
+        }
     }
 }
 
